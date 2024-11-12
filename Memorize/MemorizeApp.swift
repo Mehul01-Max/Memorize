eb837a785f447ee713d927c0acce76dbc5153c5f
Mehul Agarwal <agarwalmehul423@gmail.com>
2024-11-12 11:31:52 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mehul Agarwal on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
